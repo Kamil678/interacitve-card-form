891d86fa59166ec2c2a909aea054b17df2eeb7dd
Kamil Pigulak <kamil.pigulak@mediaflex.pl>
2022-10-07 10:39:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld />
+  <FrontCard />
+  <BackCard />
+  <Form />
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import FrontCard from "./components/FrontCard.vue";
+import BackCard from "./components/BackCard.vue";
+import Form from "./components/Form.vue";
+</script>
+
+<style scoped></style>
